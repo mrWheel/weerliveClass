2c6782c6b3da41a9c8d76d0ba2c591c0cf85c84f
Willem Aandewiel <Willem@Aandewiel.nl>
2024-07-17 17:17:24 +0200

    Add week verwachting from Weerlive

---
+++ ArduinoIDE/Weerlive/myCredentials_org.h
@@ -0,0 +1,10 @@
+#ifndef _MYCREDENTIALS_H
+#define _MYCREDENTIALS_H
+
+const char *SSID     = "YOUR_WIFI_SSID";
+const char *PASSWORD = "YOUR_WIFI_PASSWORD";
+
+const char *AUTHKEY  = "1234567";
+const char *YOURCITY = "Amsterdam";
+
+#endif // _MYCREDENTIALS_H
